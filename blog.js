278dcb6928bdaa1df4aeac82cd97a6438ba20bfa
Mayur8863 <mayor8863@gmail.com>
2022-01-08 22:40:38 +0530

    Added news

---
+++ blog.js
@@ -0,0 +1,24 @@
+var form = document.getElementById("Que");
+    
+async function handleSubmit(event) {
+  event.preventDefault();
+  var status = document.getElementById("status");
+  var data = new FormData(event.target);
+  fetch(event.target.action, {
+    method: form.method,
+    body: data,
+    headers: {
+        'Accept': 'application/json'
+    }
+  }).then(response => {
+      status.classList.add('success');
+    status.innerHTML = "Thanks for your submission!";
+    form.reset()
+  }).catch(error => {
+    status.classList.add('success');
+    status.innerHTML = "Oops! There was a problem submitting your form"
+  });
+ 
+}
+form.addEventListener("submit", handleSubmit)
+
